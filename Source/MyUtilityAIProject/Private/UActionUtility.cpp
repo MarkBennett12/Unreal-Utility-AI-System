0f998ebf168495158e8c171a3bfa4d2449cb10d8
MarkBennett12 <markusb51@hotmail.com>
2023-09-07 09:39:34 +0100

    Instance satisfaction is now a Blueprint class referenced by the Action class

---
+++ Source/MyUtilityAIProject/Private/UActionUtility.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UActionUtility.h"
+
+UActionUtility::UActionUtility()
+{
+}
+
+UActionUtility::~UActionUtility()
+{
+}
---
+++ Source/MyUtilityAIProject/Private/UInsistenceSatisfaction.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UInsistenceSatisfaction.h"
+
+UInsistenceSatisfaction::UInsistenceSatisfaction()
+{
+}
+
+UInsistenceSatisfaction::~UInsistenceSatisfaction()
+{
+}
