07157d5c3412c51b3e8516e2f3a93b7f0c115725
MarkBennett12 <markusb51@hotmail.com>
2023-11-08 20:18:13 +0000

    Insistence struct converted to blueprint class. Folder structure tidied up

---
+++ Source/MyUtilityAIProject/Private/UGoalBase.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UGoalBase.h"
+
+UGoalBase::UGoalBase()
+{
+}
+
+UGoalBase::~UGoalBase()
+{
+}
---
+++ Source/MyUtilityAIProject/Public/UGoalBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYUTILITYAIPROJECT_API UGoalBase
+{
+public:
+	UGoalBase();
+	~UGoalBase();
+};
